75c7eb17445ea73b3f0b23d69cca13236df0dbfa
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-08-11 16:23:24 +0200

    Add files via upload

---
+++ Opdracht-6-JavaScript-Function-Basic/script.js
@@ -0,0 +1,25 @@
+const makePizza = function() {
+    console.log("Make dough");
+    console.log("Add vegetables and mushrooms");
+    console.log("Put the pizza in the oven");
+}
+
+
+// Using a function
+makePizza();
+makePizza();
+makePizza();
+
+
+const makeSushi = function() {
+    console.log("Make rice");
+    console.log("Add cucumber and vegan");
+    console.log("Add ginger and wasabi to sushi");
+}
+
+// Using a function
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
