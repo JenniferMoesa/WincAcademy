e1d01b15312c692fbeed3ffe274042b791e35925
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-08-15 21:03:55 +0200

    Add files via upload

---
+++ Opdracht-7-JavaScript-Function-Arguments/script.js
@@ -0,0 +1,14 @@
+const paintWall = function(item) {
+    console.log("The wall has been painted + item");
+
+}
+
+const paint_green = "green";
+const paint_black = "black";
+
+paintWall(paint_green);
+paintWall(paint_black);
+
+const chooseWall = function(item) {
+    console.log("The + item + has been painted")
+}
