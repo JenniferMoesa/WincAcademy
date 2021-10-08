63b7cd550574909c7c05e0fd8e9e852480bc75ff
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-10-08 17:53:20 +0200

    Add files via upload

---
+++ opdracht-14-Event-Handlers-defer/script.js
@@ -0,0 +1,8 @@
+/* Opdracht 3:
+
+We gaan de changeColor functie veranderen zodat de achtergrondkleur ook weer terug veranderd naar rood wanneer er op de knop wordt geklikt.
+De achtergrondkleur wordt dan "getoggeld" , met andere woorden: erop gezet en er weer afgehaald en er weer opgezet, etc.
+Verander de naam van de functie van changeColor naar toggleColor
+In plaats van het toevoegen van een extra class "red-background" aan de classlist gaan we de class togglen.
+Gebruik Google om erachter te komen hoe je dit doet. Hint: toggle.
+*/
