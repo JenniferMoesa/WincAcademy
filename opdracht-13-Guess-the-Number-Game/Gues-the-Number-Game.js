fd1a3d3febd5473bc0e715bee6b9bd0a8e9d31a2
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-10-08 15:36:15 +0200

    Add files via upload

---
+++ opdracht-13-Guess-the-Number-Game/Gues-the-Number-Game.js
@@ -0,0 +1,20 @@
+/*Aan deze eisen moet jouw project voldoen:
+
+Als gebruiker wil ik een initieel bericht zien in die de gebruiker begroet en vraagt om zijn of haar naam. "Welkom! Wat is je naam?"
+
+Als gebruiker wil ik mijn naam kunnen invoeren in de browser en op enter drukken.
+
+Als gebruiker wil ik nadat ik mijn naam heb ingevoerd, een bericht terug zien: "Hey [naam]"
+
+Als gebruiker wil ik na de begroeting met mijn naam gevraagd worden om een nieuwe input met een cijfer. e.g: "Voer een nummer in van 0 tot 25 om te beginnen met raden..."
+
+Als spelmaker wil ik dat de gebruiker wel een reeële kans heeft om te winnen. Daarom pak ik een willekeurig getal van 0 tot 25. Hint: gebruikt Math.random()
+
+Als gebruiker wil ik een nummer in kunnen voeren en vervolgens op enter klikken.
+
+Als gebruiker wil ik een bericht zien als ik het nummer verkeerd heb geraden e.g. "Dat is niet correct" . Daarna wil ik gevraagd worden om opnieuw te raden.
+
+Als gebruiker wil ik een bericht ontvangen als ik het nummer goed heb geraden. e.g: "Gefeliciteerd je hebt gewonnen". Het spel is nu af.
+
+Als gebruiker wil ik een bericht zien als het spel af is. "e.g: Dag [naam]. Tot de volgende keer"*/
+
